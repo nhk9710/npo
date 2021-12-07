f61503fd17f2f337bb7d3a0fba2947a561022487
nhk9710 <hknoh@wkit.co.kr>
2021-12-07 17:34:02 +0900

    [add]메인페이지추가

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,237 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <div>
+    <div class="mainBox">
+      <header>
+        <div class="flex justify-end">
+          <span>페북</span>
+          <span>블로그</span>
+          <span>인스타그램</span>
+          <span>유튜브</span>
+        </div>
+        <div class="flex justify-around">
+          <div class="npoLeft">하나</div>
+          <div class="npoMiddle">둘</div>
+          <div class="npoRight">셋</div>
+        </div>
+      </header>
+
+<!-- 헤더 끝 -->
+
+
+
+      <div class="mainbody">
+        <div style="border: 1px solid black">
+          슬라이더
+        </div>
+
+        <div class="top flex justify-between">
+
+
+          <div style="width: 48%">
+            <q-item>
+              <q-item-section>
+                <q-item-label style="color: #11b466; font-weight: bold" class="text-h5">센터소식</q-item-label>
+              </q-item-section>
+              <q-item-section side>
+                <q-item-label caption>더보기<q-icon name="add" color="green-4" size="30px" /></q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-separator color="green-4" size="2px" inset/>
+
+            <q-list class="centerList q-list--dense">
+              <q-item class="centerNews">
+                <q-item-section>
+                  <q-item-label>센터소식</q-item-label>
+                </q-item-section>
+                <q-item-section>
+                  <q-item-label>2021 그룹워킹데이</q-item-label>
+                </q-item-section>
+                <q-item-section side>
+                  <q-item-label caption>2021-12-07</q-item-label>
+                </q-item-section>
+              </q-item>
+
+              <q-separator spaced inset/>
+
+              <q-item class="centerNews">
+                <q-item-section>
+                  <q-item-label>센터소식</q-item-label>
+                </q-item-section>
+                <q-item-section>
+                  <q-item-label>2021 그룹워킹데이</q-item-label>
+                </q-item-section>
+                <q-item-section side>
+                  <q-item-label caption>2021-12-07</q-item-label>
+                </q-item-section>
+              </q-item>
+
+              <q-separator spaced inset/>
+
+              <q-item class="centerNews">
+                <q-item-section>
+                  <q-item-label >센터소식</q-item-label>
+                </q-item-section>
+                <q-item-section>
+                  <q-item-label>2021 그룹워킹데이</q-item-label>
+                </q-item-section>
+                <q-item-section side>
+                  <q-item-label caption>2021-12-07</q-item-label>
+                </q-item-section>
+              </q-item>
+
+              <q-separator spaced inset/>
+
+              <q-item class="centerNews">
+                <q-item-section>
+                  <q-item-label>센터소식</q-item-label>
+                </q-item-section>
+                <q-item-section>
+                  <q-item-label>2021 그룹워킹데이</q-item-label>
+                </q-item-section>
+                <q-item-section side>
+                  <q-item-label caption>2021-12-07</q-item-label>
+                </q-item-section>
+              </q-item>
+
+              <q-separator spaced inset/>
+
+              <q-item class="centerNews">
+                <q-item-section>
+                  <q-item-label>센터소식</q-item-label>
+                </q-item-section>
+                <q-item-section>
+                  <q-item-label>2021 그룹워킹데이</q-item-label>
+                </q-item-section>
+                <q-item-section side>
+                  <q-item-label caption>2021-12-07</q-item-label>
+                </q-item-section>
+              </q-item>
+
+              <q-separator spaced inset/>
+
+            </q-list>
+          </div>
+
+
+
+          <div style="width: 48%">
+            <q-item>
+              <q-item-section>
+                <q-item-label style="font-weight: bold" class="text-h5">NPO소식</q-item-label>
+              </q-item-section>
+              <q-item-section side>
+                <q-item-label caption>더보기<q-icon name="add" color="purple-9" size="30px" /></q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-separator color="purple-9" size="2px" inset/>
+            <div>
+              <q-list class="npoList q-list--dense">
+                <q-item class="npoNews">
+                  <q-item-section>
+                    <q-item-label>NPO소식</q-item-label>
+                  </q-item-section>
+                  <q-item-section>
+                    <q-item-label>2021 그룹워킹데이</q-item-label>
+                  </q-item-section>
+                  <q-item-section side>
+                    <q-item-label caption>2021-12-07</q-item-label>
+                  </q-item-section>
+                </q-item>
+
+                <q-separator spaced inset/>
+
+                <q-item class="npoNews">
+                  <q-item-section>
+                    <q-item-label>구인</q-item-label>
+                  </q-item-section>
+                  <q-item-section>
+                    <q-item-label>2021 그룹워킹데이</q-item-label>
+                  </q-item-section>
+                  <q-item-section side>
+                    <q-item-label caption>2021-12-07</q-item-label>
+                  </q-item-section>
+                </q-item>
+
+                <q-separator spaced inset/>
+
+                <q-item class="npoNews">
+                  <q-item-section>
+                    <q-item-label >구인</q-item-label>
+                  </q-item-section>
+                  <q-item-section>
+                    <q-item-label>2021 그룹워킹데이</q-item-label>
+                  </q-item-section>
+                  <q-item-section side>
+                    <q-item-label caption>2021-12-07</q-item-label>
+                  </q-item-section>
+                </q-item>
+
+                <q-separator spaced inset/>
+
+                <q-item class="npoNews">
+                  <q-item-section>
+                    <q-item-label>NPO소식</q-item-label>
+                  </q-item-section>
+                  <q-item-section>
+                    <q-item-label>2021 그룹워킹데이</q-item-label>
+                  </q-item-section>
+                  <q-item-section side>
+                    <q-item-label caption>2021-12-07</q-item-label>
+                  </q-item-section>
+                </q-item>
+
+                <q-separator spaced inset/>
+
+                <q-item class="npoNews">
+                  <q-item-section>
+                    <q-item-label>NPO소식</q-item-label>
+                  </q-item-section>
+                  <q-item-section>
+                    <q-item-label>2021 그룹워킹데이</q-item-label>
+                  </q-item-section>
+                  <q-item-section side>
+                    <q-item-label caption>2021-12-07</q-item-label>
+                  </q-item-section>
+                </q-item>
+
+                <q-separator spaced inset/>
+
+
+
+              </q-list>
+            </div>
+
+          </div>
+        </div>
+
+        <div class="flex justify-between">
+          <div>
+            <span class="text-h5 text-weight-bold">아카이브</span>
+          </div>
+
+
+          <div>
+            <span class="text-h5 text-weight-bold">사진</span>
+          </div>
+        </div>
+
+        <div style="text-align: center">
+          <span class="text-h5 text-weight-bold">NPO즐겨찾기</span>
+        </div>
+
+        <div class="newsBox">
+          <span class="text-h5 text-weight-bold">뉴스레터를 받아보세요</span>
+        </div>
+
+      </div>
+
+
+
+      <footer class="footer">
+        <img src="/src/img/ico_ci_02.jpg"/>
+      </footer>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -13,3 +239,50 @@ export default {
   name: 'PageIndex'
 }
 </script>
+
+<style>
+.mainBox{
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+  border: 1px solid black;
+}
+
+.top{
+  margin-bottom: 20px;
+}
+
+.newsBox{
+  background: #f5f5f5;
+  text-align: center;
+}
+.centerList{
+
+
+}
+.centerNews{
+  color: #11b466;
+  font-weight: bold;
+}
+
+.npoList{
+
+}
+.npoNews{
+  color: #615097;
+  font-weight: bold;
+}
+
+li{
+  list-style: none;
+}
+
+q-separator{
+  margin-top: 0;
+}
+
+footer{
+  background: #333333;
+  color: #ffffff;
+}
+</style>
